36af2661ef088b30b3d0e7c01d36b3f320b3bffa
Estefano Campana <Estefano.CampanaMunoz@edu.sait.ca>
2025-03-18 10:09:08 -0600

    Add project files.

---
+++ BasicMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath
+{
+    public class BasicMath
+    {
+        public static int Add(int number, int adding)
+        {
+            return number + adding;
+        }
+        public static int Substrat(int number, int subtracting)
+        {
+            return number - subtracting;
+        }
+        public static int Multiply(int number, int multiplier) 
+        {
+            return number * multiplier; 
+        }
+        public static int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
